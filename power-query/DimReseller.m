4335a3f1caf32b60a8d29a37dc94d59ad17fdf40
Ankur Kumar Jaiswal <117014504+rjraunak04@users.noreply.github.com>
2026-09-19 21:24:02 +0530

    fix: restore Stage 5 proven Power Query sources

--- power-query/DimReseller.m
+++ power-query/DimReseller.m
@@ -1,11 +1,5 @@
 let
-    Source = PostgreSQL.Database(
-        pServer,
-        pDatabase,
-        [
-            CreateNavigationProperties=false,
-            Query="SELECT reseller_key, reseller_code, reseller_name, business_type, number_employees, order_frequency, order_month, first_order_year, last_order_year, product_line, annual_sales, annual_revenue, year_opened FROM analytics.dim_reseller"
-        ]
-    )
+    Source = PostgreSQL.Database(pServer, pDatabase, [CreateNavigationProperties=false]),
+    Analytics = Source{[Schema="analytics", Item="dim_reseller"]}[Data]
 in
-    Source
+    Analytics
